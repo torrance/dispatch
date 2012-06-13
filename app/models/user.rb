2fb04e1f472e17a9fb1fa3f04c1a747efd6732ab
Torrance <torrance123@gmail.com>
2012-06-13 21:21:36 +1200

    Set email field to be case insensitive.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation, :biography, :display_name
+
   has_many :articles
 
   acts_as_authentic do |c|
-    # Configuration
+    # Drupal used plain md5 passwords with no salt, so we are able to
+    # seamlessly transition to using SHA512 by setting md5 as the fallback
+    # crypto provider.
+    c.transition_from_crypto_providers Authlogic::CryptoProviders::MD5
+    c.crypto_provider Authlogic::CryptoProviders::Sha512
+  end
+
+  def email=(email)
+    write_attribute(:email, email.downcase)
+  end
+
+  def self.find_by_email(email)
+    self.where(email.downcase)
   end
 end
