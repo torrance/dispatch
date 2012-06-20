6f55567ffe43c71ba23dfc3a0bbd94da7c9b3849
Torrance <torrance123@gmail.com>
2012-06-20 19:14:53 +1200

    Create placeholder front page listing 30 most recent articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params[:article])
+    @article.user = current_user
 
     if @article.save
       redirect_to @article, notice: 'Your article was successfully created.'
--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,4 +1,5 @@
 class FrontpageController < ApplicationController
   def show
+    @articles = Article.order("created_at DESC").limit(30)
   end
 end
