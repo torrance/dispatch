951dae6e72e325a29b0134d504632615c58a2b87
Torrance <torrance123@gmail.com>
2012-06-23 16:47:06 +1200

    Initial theming of articles pages.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ActiveRecord::Base
   attr_accessible :image
   belongs_to :article
 
-  has_attached_file :image, :styles => { :article => "720x480", }
+  has_attached_file :image, :styles => { :article => ["700x467", :jpg] }
 end
