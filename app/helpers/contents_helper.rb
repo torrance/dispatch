7d7e3818dd0d6441faa47ce967d9c5d02c68d78b
Torrance <torrance123@gmail.com>
2012-06-28 21:48:24 +1200

    Format event dates to be human.

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -14,7 +14,30 @@ module ContentsHelper
   def long_event_date(date)
     # Grab the time, with the minutes displayed only if they're off the hour.
     time = pretty_hour(date)
-    date.strftime("#{time}, %A #{date.day.ordinalize} %B %Y")
+
+    today = Date.today
+    # Event has been
+    if date.past?
+      date.strftime "#{time}, #{date.day.ordinalize} %B %Y"
+    # Event is today!
+    elsif date.today?
+      date.strftime "Today, #{time}"
+    # Event is tomorrow
+    elsif today.tomorrow === date.to_date
+      date.strftime "Tomorrow, #{time}"
+    # Event is happening this week
+    elsif date < today.end_of_week
+      date.strftime "#{time}, %A"
+    # Event is happening this month, but not this week.
+    elsif date < today.end_of_month
+      date.strftime "#{time}, #{date.day.ordinalize} %A"
+    # Event  is less than 3 months away, but not happening this month.
+    elsif date < today.months_since(3)
+      date.strftime "#{time}, %A #{date.day.ordinalize} %B"
+    # Event has been or is more than 3 months away
+    else
+      date.strftime "#{time}, #{date.day.ordinalize} %B %Y"
+    end
   end
 
   def pretty_hour(datetime)
