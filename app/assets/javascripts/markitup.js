a620a0ed5293dee58dbf0254f56f42f2f0d4f07a
Torrance <torrance123@gmail.com>
2012-07-01 12:30:39 +1200

    Remove unwanted markdown buttons from markitup editor.

--- app/assets/javascripts/markitup.js
+++ app/assets/javascripts/markitup.js
@@ -4,12 +4,8 @@ $(function() {
     onShiftEnter:   {keepDefault:false, openWith:'\n\n'},
     afterInsert: function() { $(".markitup").keyup(); },
     markupSet: [
-      {name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-      {name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-      {name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-      {name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-      {name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-      {name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+      {name:'Subheading', key:'2', openWith:'## ', placeHolder:'Your title here...' },
+      {name:'Sub-subheading', key:'3', openWith:'### ', placeHolder:'Your title here...' },
       {separator:'---------------' },   
       {name:'Bold', key:'B', openWith:'**', closeWith:'**'},
       {name:'Italic', key:'I', openWith:'_', closeWith:'_'},
@@ -19,24 +15,9 @@ $(function() {
         return markItUp.line+'. ';
       }},
       {separator:'---------------' },
-      {name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
       {name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
       {separator:'---------------'},  
-      {name:'Quotes', openWith:'> '},
-      {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-      {separator:'---------------'},
+      {name:'Blockquote', openWith:'> '},
     ]
   });
 });
-
-// mIu nameSpace to avoid conflict.
-miu = {
-  markdownTitle: function(markItUp, char) {
-    heading = '';
-    n = $.trim(markItUp.selection||markItUp.placeHolder).length;
-    for(i = 0; i < n; i++) {
-      heading += char;
-    }
-    return '\n'+heading;
-  }
-}
