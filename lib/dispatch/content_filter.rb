4f680176f230c96bb4b981f1559bcd3cb83cd469
Torrance <torrance123@gmail.com>
2012-07-01 12:15:27 +1200

    Move rendering configuration values to renderer.

--- lib/dispatch/content_filter.rb
+++ lib/dispatch/content_filter.rb
@@ -3,10 +3,11 @@ module Dispatch
     include Singleton
 
     def initialize
-      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-        :filter_html => true, :no_images => true, :hard_wrap => true,
-        :auto_link => true, :no_intra_emphasis => true,
-        :space_after_headers => true, :superscript => true)
+      @markdown = Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML.new(:filter_html => true, :no_images => true, :hard_wrap => true),
+        :autolink => true, :no_intra_emphasis => true,
+        :space_after_headers => true, :superscript => true
+      )
     end
 
     def render(text)
