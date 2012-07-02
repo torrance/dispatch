6ace66b4b4f08b7e1030538e2b1719feeb8d59a6
Torrance <torrance123@gmail.com>
2012-07-02 21:23:20 +1200

    Add moderation state to contents model.

--- app/models/content.rb
+++ app/models/content.rb
@@ -17,8 +17,6 @@ class Content < ActiveRecord::Base
   accepts_nested_attributes_for :images, :allow_destroy => true,
     :reject_if => proc { |image_attributes| image_attributes[:id].blank? && image_attributes[:image].blank? }
 
-  default_scope :include => :user, :include => :images
-
   # Validations
   validates :title, :summary, :body, :presence => true
   validates :title, :length => { :maximum => 255 }
@@ -26,6 +24,14 @@ class Content < ActiveRecord::Base
   validate :require_author
   validate :pseudonym,  :length => { :maximum => 40 },
     :message => "^Author name is too long (maximum length 40 characters)."
+
+  # Scopes
+  default_scope :include => :user, :include => :images
+
+  scope :hidden, where(:moderation => 0)
+  scope :normal, where(:moderation => 1)
+  scope :featured, where(:moderation => 2)
+
 
   # Public model methods
   def require_author
@@ -35,9 +41,9 @@ class Content < ActiveRecord::Base
   end
   
   def author_name
-    if not user.blank?
+    if user
       user.display_name
-    elsif not pseudonym.blank?
+    elsif pseudonym
       pseudonym
     else
       "Anonymous"
@@ -48,15 +54,38 @@ class Content < ActiveRecord::Base
     Time.now - created_at > 1.week
   end
 
+  def is_author?(user)
+    user && self.user == user
+  end
+
   def editable?(user)
-    if user.blank?
-      false
-    elsif user.is_editor?
-      true
-    elsif user == self.user and not locked?
-      true
-    else
-      false
-    end
+    user && (user.is_editor? || is_author?(user) && !locked?)
+  end
+
+  def hidden
+    moderation = 0
+    self
+  end
+
+  def normal
+    moderation = 1
+    self
+  end
+
+  def featured
+    moderation = 2
+    self
+  end
+
+  def is_hidden?
+    moderation == 0
+  end
+
+  def is_normal?
+    moderation == 1
+  end
+
+  def is_featured?
+    moderation == 2
   end
 end
