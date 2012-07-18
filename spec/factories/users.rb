724628309813247a90df2237999b8a07ac2dd885
Torrance <torrance123@gmail.com>
2012-07-18 21:56:23 +1200

    Ensure user bios are joined into single string in user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :user do
     display_name { Faker::Name.name }
     email { Faker::Internet.email }
-    biography { Faker::Lorem.sentences 2 }
+    biography { Faker::Lorem.sentences(rand 0..4).join(' ') }
     password "password"
 
     factory :active_user do
