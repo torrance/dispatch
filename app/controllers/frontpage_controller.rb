4c12553dfcc509f399603b67e0fa085eab82f09f
Torrance <torrance123@gmail.com>
2012-07-18 13:30:18 +1200

    Modify front page feature layout.

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,7 +1,7 @@
 class FrontpageController < ApplicationController
   def show
     @primary_feature = Content.recent.has_image.featured.first
-    @features = Content.recent.featured.where("id <> ?", @primary_feature.id).limit(6)
+    @features = Content.recent.featured.where("id <> ?", @primary_feature.id).limit(7)
     @newswire = Content.recent.where("id <> ?", @primary_feature).
       where("id NOT IN (?)", @features.map(&:id)).page(params[:page]).per(25)
     @events = Event.upcoming
