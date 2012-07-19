8403dd9965161f53491088be956523ff60484e28
Torrance <torrance123@gmail.com>
2012-07-19 14:29:25 +1200

    Fix pluralization for user stats.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,11 +5,11 @@ module UsersHelper
     comments = user.comments.count
 
     if items > 0 && comments > 0
-      "#{date_joined} Has published <strong>#{items} items</strong> and has made <strong>#{comments} comments</strong>.".html_safe
+      "#{date_joined} Has published <strong>#{pluralize items, "item"}</strong> and has made <strong>#{pluralize comments, "comment"}</strong>.".html_safe
     elsif items > 0
-      "#{date_joined} Has published <strong>#{items} items</strong> but hasn't made any comments yet.".html_safe
+      "#{date_joined} Has published <strong>#{pluralize items, "item"}</strong> but hasn't made any comments yet.".html_safe
     elsif comments > 0
-      "#{date_joined} Has has made <strong>#{comments} comments</strong> but has yet published any items.".html_safe
+      "#{date_joined} Has has made <strong>#{pluralize comments, "comment"}</strong> but has yet published any items.".html_safe
     else
       "#{date_joined} Hasn't yet published any items or comments.".html_safe
     end
