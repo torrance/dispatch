3d32574d9f5f6d847eda517492778a38ea569e0b
Torrance <torrance123@gmail.com>
2012-07-22 14:22:41 +1200

    Modify fade behaviour of publish panel to hide when mouse leaves.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,21 +1,35 @@
 $(function() {
   $('body').removeClass('no-js');
 
+  /**
+   * Control visibility of publish panel
+   */
   var publishButton = $('#publish ul');
-  $('#publish a.button').toggle(
+  $('#publish a.button').click(
     function() {
       publishButton.fadeIn('fast');
-    },
-    function() {
-      publishButton.fadeOut('fast');
     }
   );
+  publishButton.mouseleave(function() {
+    $(this).fadeOut('slow');
+  })
+  publishButton.mouseenter(function() {
+    $(this).fadeIn('fast');
+  })
 
+  /**
+   * Allow users to expand newswire items to view full summary and image.
+   */
   $('.newswire-item .collapsed .summary').live('click', function() {
     $(this).parent().removeClass('collapsed');
   });
 
-  // Hide the pager once it has been clicked.
+  /**
+   * Hide the pager once it has been clicked
+   *
+   * We use 'live' to ensure subsequent pagers loaded via ajax also receive the
+   * the same treatment.
+   */
   $('#pager').live('click', function() {
     $(this).animate({ opacity: 0 }, {
       duration: 'fast',
