edf74dbd2f9a530ba8dce0fa531890aca254acba
Torrance <torrance123@gmail.com>
2012-07-22 12:46:25 +1200

    Implement user edit page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+
+    if @user.save
+      redirect_to @user, :notice => 'Your user settings have been successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
