337c160496897e8e711c33e04393ba399d4d7b9e
Torrance <torrance123@gmail.com>
2012-07-25 21:31:55 +1200

    Use ranges compatible with 1.9.2.

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -5,13 +5,13 @@ FactoryGirl.define do
     #include ActionDispatch::TestProcess
 
     title { Faker::Lorem.sentence.gsub(/\.$/, '') }
-    summary { Faker::Lorem.sentences(rand 3..6).join(' ')[0..300] }
-    body { Faker::Lorem.paragraphs(rand 1..10).join("\n\n") }
+    summary { Faker::Lorem.sentences(3 + rand 3).join(' ')[0..300] }
+    body { Faker::Lorem.paragraphs(1 + rand 10).join("\n\n") }
     category { Article::CATEGORIES.sample }
-    tag_list { Faker::Lorem.words(rand 1..5).split.join(', ') }
+    tag_list { Faker::Lorem.words(1 + rand 5).split.join(', ') }
     pseudonym { Faker::Name.name }
     status { [2, 2, 2, 3].sample }
-    created_at { DateTime.now - rand(0..100).days }
+    created_at { DateTime.now - rand(100).days }
     images do
       random_images = Dir.new('spec/factories/images').each.to_a.delete_if{ |f| f == '.' || f == '..' }.sample([0, 0, 0, 0, 1, 3, 5].sample)
       random_images.map do |image|
