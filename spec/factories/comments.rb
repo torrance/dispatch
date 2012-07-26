fbebb9325effcfa7dfcb4186ef989fc79f6b82a8
Torrance <torrance123@gmail.com>
2012-07-26 21:44:40 +1200

    Set comment factory to create comments dated after the parent article.

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :comment do
     body { Faker::Lorem.paragraphs([1, 1, 1 + rand(4)].sample).join("\n\n") }
     content { content }
-    created_at { content.created_at + rand(8).days + rand(24).hours }
+    created_at { content.created_at - rand(8).days - rand(24).hours }
 
     factory :comment_with_existing_user do
       user { User.all.sort_by{ rand }.first }
