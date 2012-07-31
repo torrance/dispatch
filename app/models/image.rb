3505232e0f08b5cd233883250132f9858e6e1729
Torrance <torrance123@gmail.com>
2012-07-31 18:30:09 +1200

    Set image preset sizes to final size, and rename category_feature to sub_feature.

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,12 +7,11 @@ class Image < ActiveRecord::Base
   default_scope order('weight ASC')
 
   has_attached_file :image, :styles => {
-    :article => ["700x467", :jpg],
-    :tiny => ["60x60", :jpg],
-    :gallery_thumbnail => ["157x157#", :jpg],
-    :gallery_full => ["900x700", :jpg],
-    :primary_feature => ["605x350#", :jpg],
-    :feature_square => ["100x100#", :jpg],
-    :category_feature => ["235x150#", :jpg]
+    :article => ["695x470", :jpg],
+    :gallery_thumbnail => ["140x140#", :jpg],
+    :gallery_full => ["900x900", :jpg],
+    :primary_feature => ["510x300#", :jpg],
+    :feature_square => ["110x110#", :jpg],
+    :sub_feature => ["235x150#", :jpg]
   }
 end
