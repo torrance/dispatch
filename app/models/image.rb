e48d9a9d9cb0a36b5d9e88ca53326d2d71533ed6
Torrance <torrance123@gmail.com>
2012-08-18 10:07:09 +1200

    Don't upscale images.

--- app/models/image.rb
+++ app/models/image.rb
@@ -15,9 +15,9 @@ class Image < ActiveRecord::Base
   default_scope order('weight ASC')
 
   has_attached_file :image, :styles => {
-    :article => ["695x470", :jpg],
+    :article => ["695x470>", :jpg],
     :gallery_thumbnail => ["140x140#", :jpg],
-    :gallery_full => ["900x900", :jpg],
+    :gallery_full => ["900x900>", :jpg],
     :primary_feature => ["510x300#", :jpg],
     :feature_square => ["110x110#", :jpg],
     :sub_feature => ["235x150#", :jpg],
