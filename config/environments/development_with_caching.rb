7a417ebb1a117d7fb295a385816d63a1bab0bcb2
Torrance <torrance123@gmail.com>
2012-10-07 14:58:52 +1300

    Use Mailcatcher to mail delivery on dev.

--- config/environments/development_with_caching.rb
+++ config/environments/development_with_caching.rb
@@ -15,7 +15,10 @@ Dispatch::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port => 1025 # Mailcatcher default port
+  }
   config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
 
   # Print deprecation notices to the Rails logger
