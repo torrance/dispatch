c0e686b720bea0b2e3bd484240c7ac4b0b55eb57
Torrance <torrance123@gmail.com>
2012-11-13 10:05:45 +1300

    Increase strictness of spam test by requiring a definite language after more than 30 words.

--- lib/language_validator.rb
+++ lib/language_validator.rb
@@ -8,8 +8,10 @@ class LanguageValidator < ActiveModel::EachValidator
     end
 
     # It's possible multiple languages may be detected. We're OK with this
-    # so long as the desired language is among them.
-    unless languages.empty? || !languages[options[:language].to_s].nil?
+    # so long as the desired language is among them. If there are fewer than 30 words,
+    # we don't require a definite answer.
+    word_count = value.split.size
+    if word_count > 30 && languages[options[:language].to_s].nil?
       record.errors[attribute] << (options[:message] || "is not the correct language")
     end
   end
