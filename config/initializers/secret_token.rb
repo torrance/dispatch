321820e65c51b890c50e7f5e90bdd46893110ee4
Torrance <torrance123@gmail.com>
2013-06-02 12:06:10 +1200

    Set secret_token via environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dispatch::Application.config.secret_token = '4ddce3ac17be5e199b490397a6e3c15bfcf39e98b1664b0b59d17df0ffc673553f85e80f80b26c8fccff115cccd80dc6d833983acf5b9a45f83bd27e3844ae9a'
+secret = ENV['RAILS_SECRET_TOKEN'].nil? ? "" : ENV['RAILS_SECRET_TOKEN']
+if secret.length < 30
+  raise "Secret token cannot be loaded"
+else
+  Dispatch::Application.config.secret_token = secret
+end
