19170f3435d3b7f8f417cbb635be61e68e549de2
Torrance <torrance123@gmail.com>
2013-06-02 12:52:22 +1200

    Temporarily set secret token during capistrano deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 set :application, "dispatch"
 
-
 set :use_sudo, false
 set :user, 'indymedia'
 set :deploy_to, "/var/www/www.indymedia.org.nz"
@@ -18,8 +17,10 @@ role :db, "flax.indymedia.org.nz"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 set :rake,  "/var/lib/gems/1.9.1/bin/rake"
+# This secret token is only temporarily used during deployment. Otherwise it is set by environment variable.
+set :secret_token, 'd12a472fad0423d80856cb03ee5b4999cde3a6e0d48bd247984e3d0fde1b51f1c0269a024555371d3d9fac89ce805c3700ae848f81f703ccad3b122ea4923608'
 after "deploy:update_code" do
-  run "cd #{release_path}; RAILS_ENV=production #{rake} assets:precompile; RAILS_ENV=production #{rake} db:migrate"
+  run "cd #{release_path}; RAILS_ENV=production RAILS_SECRET_TOKEN=#{secret_token} #{rake} assets:precompile; RAILS_ENV=production RAILS_SECRET_TOKEN=#{secret_token} #{rake} db:migrate"
 end
 
 namespace :deploy do
