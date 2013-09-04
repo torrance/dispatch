664dddfadf45f5b53626a98622eb9ffca0992ac8
Torrance <torrance123@gmail.com>
2013-09-04 12:13:06 +1200

    Create valid article factory.

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -5,12 +5,12 @@ FactoryGirl.define do
     #include ActionDispatch::TestProcess
 
     title { Faker::Lorem.sentence.gsub(/\.$/, '') }
-    summary { Faker::Lorem.sentences(3 + rand(3)).join(' ')[0..300] }
+    summary { Faker::Lorem.sentences(3 + rand(3)).join(' ')[0..254] }
     body { Faker::Lorem.paragraphs(1 + rand(10)).join("\n\n") }
     category { Article::CATEGORIES.sample }
     tag_list { Faker::Lorem.words(1 + rand(5)).split.join(', ') }
     pseudonym { Faker::Name.name }
-    status { [1, 1, 1, 2, 2, 3].sample }
+    status { [0, 0, 0, 0, 1, 2, 2].sample }
     created_at { DateTime.now - rand(730).days }
     images do
       random_images = Dir.new('spec/factories/images').each.to_a.delete_if{ |f| f == '.' || f == '..' }.sample([0, 0, 0, 0, 1, 3, 5].sample)
@@ -19,7 +19,7 @@ FactoryGirl.define do
       end
     end
 
-    factory :with_existing_user do
+    factory :article_with_existing_user do
       user { User.all.sort_by{ rand }.first }
     end
   end
