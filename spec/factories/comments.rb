7cf2847d52a5d81a54e57aeaf090e1d15ddb5aee
Torrance <torrance123@gmail.com>
2013-09-04 12:13:34 +1200

    Add status to comment factory.

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     body { Faker::Lorem.paragraphs([1, 1, 1 + rand(4)].sample).join("\n\n") }
     content { content }
     created_at { content.created_at - rand(8).days - rand(24).hours }
+    status { [1, 1, 1, 1, 1, 1, 1, 0].sample }
 
     factory :comment_with_existing_user do
       user { User.all.sort_by{ rand }.first }
