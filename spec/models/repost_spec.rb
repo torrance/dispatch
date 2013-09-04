86d11b45c1b8c58615f349d5af5009d29b94ea5a
Torrance <torrance123@gmail.com>
2013-09-04 18:17:47 +1200

    Add tests for repost permission.

--- spec/models/repost_spec.rb
+++ spec/models/repost_spec.rb
@@ -15,4 +15,14 @@ describe Repost do
     repost = build(:repost, url_name: "i" * 600)
     expect(repost).to have(1).errors_on(:url_name)
   end
+
+  it "has an empty body if there's no repost permission" do
+    repost = create(:repost, repost_permission: false)
+    expect(repost.body).to eq('')
+  end
+
+  it "has a body if repost permission is granted" do
+    repost = create(:repost, repost_permission: true)
+    expect(repost.body).to_not eq('')
+  end
 end
