72c1293f786eb3e53c79ebe355aa3b6d7ee40d56
Torrance <torrance123@gmail.com>
2013-09-04 17:29:26 +1200

    Separate article factory into content parent to be reused amongst other content subclasses.

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,6 +1,22 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :content do
+    title { Faker::Lorem.sentence.gsub(/\.$/, '') }
+    summary { Faker::Lorem.sentences(3 + rand(3)).join(' ')[0..254] }
+    body { Faker::Lorem.paragraphs(1 + rand(10)).join("\n\n") }
+    category { Article::CATEGORIES.sample }
+    tag_list { Faker::Lorem.words(1 + rand(5)).split.join(', ') }
+    pseudonym { Faker::Name.name }
+    status { [0, 0, 0, 0, 1, 2, 2].sample }
+    created_at { DateTime.now - rand(730).days }
+    images do
+      random_images = Dir.new('spec/factories/images').each.to_a.delete_if{ |f| f == '.' || f == '..' }.sample([0, 0, 0, 0, 1, 3, 5].sample)
+      random_images.map do |image|
+        Image.new(:image => File.open('spec/factories/images/' + image))
+      end
+    end
   end
 end
